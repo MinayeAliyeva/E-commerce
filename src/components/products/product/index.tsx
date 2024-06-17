a233bd8d266d0043ab4a19f7b933d12e54f7c047
MinayeAliyeva <7s346pu@code.edu.az>
2024-06-17 19:13:57 +0400

    notifications added

--- src/components/products/product/index.tsx
+++ src/components/products/product/index.tsx
@@ -1,32 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Col, Row, Button, Divider, notification } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
 import { Iproduct } from "../modules";
 import { useBasketContext } from "../../../contextApi/BasketContext";
-// import { connect } from "react-redux";
-// import { withRouter } from "react-router-dom";
-const Product: React.FC<{ item: Iproduct }> = ({ item }) => {
-  // const addCart = (item) => {
-  //   props.add_cart(item);
-  //   openNotification();
-  // };
-  const { addToCard, removeProduct } = useBasketContext();
-  // console.log("context", context);
 
-  // const openNotification = () => {
-  //   notification.open({
-  //     style: {
-  //       color: "#1DA57A",
-  //       fontWeight: "bold",
-  //       opacity: 0.9,
-  //       cursor: "pointer",
-  //     },
-  //     placement: "bottomRight",
-  //     message: "Item Added",
-  //     // description: `${props.itemName} is added to your cart.`,
-  //     duration: 4,
-  //   });
-  // };
+const Product: React.FC<{ item: Iproduct }> = ({ item }) => {
+  const { addToCard, removeProduct, products } = useBasketContext();
+  console.log("products", products);
+
+  const [isInCart, setIsInCart] = useState(false);
+
+  useEffect(() => {
+    const productInCart = products?.some((product) => product?.id === item?.id);
+    setIsInCart(productInCart);
+  }, [products, item?.id]);
+
+  const handleAddToCart = () => {
+    addToCard(item);
+    setIsInCart(true);
+    notification.success({ message: `${item.id} id-li product sepetde` });
+  };
+
+  // Ürünü sepetten çıkar ve durumu güncelle
+  const handleRemoveFromCart = () => {
+    removeProduct(item.id);
+    setIsInCart(false);
+    notification.success({ message: `${item.id} id-li product sepetden silindi` });
+  };
 
   return (
     <Col md={8}>
@@ -37,8 +37,7 @@ const Product: React.FC<{ item: Iproduct }> = ({ item }) => {
           <img height="320px" width="280px" alt="example" src={item?.image} />
         }
       >
-        {/* <Card.Meta title={<h2>Name</h2>} description={item.description} /> */}
-        <br></br>
+        <br />
         <Divider orientation="center">Price</Divider>
         <p
           style={{
@@ -49,29 +48,26 @@ const Product: React.FC<{ item: Iproduct }> = ({ item }) => {
             textAlign: "center",
           }}
         >
-          Price
+          {item.price} TL
         </p>
         <Row gutter={[10, 10]} className="add-cart-btn-row">
           <Col>
             <Button
-              title="Add item to cart"
-              onClick={() => {
-                addToCard(item);
-              }}
+              title="Ürünü sepete ekle"
+              onClick={handleAddToCart}
               type="primary"
+              disabled={isInCart}
             >
-              Add to cart
+              Sepete ekle
             </Button>
           </Col>
           <Col>
             <Button
-              title="Remove item from cart"
-              onClick={() => {
-                removeProduct(item?.id);
-                console.log("item?.id", item?.id);
-              }}
+              title="Ürünü sepetten çıkar"
+              onClick={handleRemoveFromCart}
               type="primary"
               danger
+              disabled={!isInCart}
             >
               <DeleteOutlined />
             </Button>
@@ -81,17 +77,5 @@ const Product: React.FC<{ item: Iproduct }> = ({ item }) => {
     </Col>
   );
 };
-
-// const mapStateToProps = (state) => ({
-//   cart: state.cart,
-//   url_key: state.url_key,
-// });
-
-// const mapDispatchToProps = (dispatch) => ({
-//   add_cart: (item) => dispatch({ type: "ADD_PRODUCT", payload: item }),
-//   remove_single: (itemId) =>
-//     dispatch({ type: "REMOVE_SINGLE", payload: itemId }),
-//   setUrl: (urlKey) => dispatch({ type: "SET_URL", payload: urlKey }),
-// });
 
 export default Product;
